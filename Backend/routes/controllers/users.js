145fd3a16f32baff0b386eca21faf2f51458ea70
SolabComputer <thomson861106@gmail.com>
2021-05-11 22:12:29 +0800

    Backend Update

--- Backend/routes/controllers/users.js
+++ Backend/routes/controllers/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const db = require(global.__MODULE_BASE__ + "database");
 const response = require(global.__MODULE_BASE__ + "response")
-const User  = require(global.__MODEL_BASE__ + "User")
+const User = require(global.__MODEL_BASE__ + "User")
 const httpStatus = require('http-status-codes');
 const aurthor = require('./Aurthorize');
 var router = express.Router();
@@ -14,46 +14,46 @@ router.post('/create', async (req, res) => {
     const password = req.body.password || "";
     const passwordConfirm = req.body.passwordConfirm || "";
     const email = req.body.email || "";
-    const adim  = req.body.adim || "public";
+    const adim = req.body.adim || "public";
     const department = req.body.department || "";
-    try{
+    try {
         const result = await new User().create(account, username, password, passwordConfirm,
             adim, email, department);
         return response.succ(res, result);
-    }catch(err){
+    } catch (err) {
         return response.fail(res, err);
     }
 })
 
 router.post('/active', async (req, res) => {
-  const user_id = req.body.user_id;
-  try{
+    const user_id = req.body.user_id || "";
+    try {
         const result = await new User().active(user_id);
         return response.succ(res, result);
-    }catch(err){
+    } catch (err) {
         return response.fail(res, err);
     }
 })
 
 router.get('/data', async (req, res) => {
     const user_id = Number(req.query.id) || '';
-    try{
+    try {
         const result = await new User(req.body.token).getUserbyId(user_id);
         return response.succ(res, result);
-    }catch(err){
+    } catch (err) {
         return response.fail(res, err);
     }
 })
 
 router.put('/login', async (req, resp) => {
-  const account = req.body.account;
-  const pwd = req.body.password;
-  try{
-      const result = await new User().login(account, pwd);
-      return response.succ(resp, result);
-  }catch(err){
-      return response.fail(resp, err);
-  }
+    const account = req.body.account;
+    const pwd = req.body.password;
+    try {
+        const result = await new User().login(account, pwd);
+        return response.succ(resp, result);
+    } catch (err) {
+        return response.fail(resp, err);
+    }
 });
 
 module.exports = router;
--- Backend/routes/models/User.js
+++ Backend/routes/models/User.js
@@ -1,16 +1,16 @@
 const _ = require('underscore');
 const jwt = require('jsonwebtoken');
-const db =  require(global.__MODULE_BASE__ + 'database');
+const db = require(global.__MODULE_BASE__ + 'database');
 const Logger = require(global.__MODULE_BASE__ + 'logger');
 const exception = require(global.__MODULE_BASE__ + 'exception');
-const tool =  require(global.__MODULE_BASE__ + 'tool');
+const tool = require(global.__MODULE_BASE__ + 'tool');
 const config = require(global.__MODULE_BASE__ + 'config');
 
-class User{
-    constructor(token){
+class User {
+    constructor(token) {
         this.token = token;
     }
-    static async getUserInfo(account){
+    static async getUserInfo(account) {
         const logger = new Logger();
         const TAG = '[GetUserInfo]';
 
@@ -35,21 +35,21 @@ class User{
 
         return result[0];
     }
-    static generateToken(userObj){
+    static generateToken(userObj) {
         // gen token
         const iat = Math.floor(Date.now() / 1000);
         const expTime = 5 * 60 * 60; // 5 hours = 60 (seconds) * 60 * (min) = 3600 * 5
         const payload = {
-            user_id : userObj.user_id,
-            account : userObj.account,
-            username : userObj.username,
-            email : userObj.email,
-            active : userObj.active,
+            user_id: userObj.user_id,
+            account: userObj.account,
+            username: userObj.username,
+            email: userObj.email,
+            active: userObj.active,
             adim: userObj.adim,
-            iat : iat,
-            exp : iat + expTime
+            iat: iat,
+            exp: iat + expTime
         };
-        
+
         // sign token
         const signedToken = jwt.sign(
             payload,
@@ -63,16 +63,16 @@ class User{
 User.prototype.create = async (account, username, password, passwordConfirmed, adim, email, deparment) => {
     const TAG = '[UserCreate]';
     const logger = new Logger();
-    if (!tool.isValidAccount(account) ){
+    if (!tool.isValidAccount(account)) {
         logger.error(TAG, `Invalid account : ${account}`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if (!tool.isValidMail(email)){
+    } else if (!tool.isValidMail(email)) {
         logger.error(TAG, `Invalid email : ${email}`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if (!tool.isValidPwd(password) || password != passwordConfirmed){
+    } else if (!tool.isValidPwd(password) || password != passwordConfirmed) {
         logger.error(TAG, `Invalid pwd : ${password}, pwdConfirmed: ${passwordConfirmed}.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if (!tool.isValidUserName(username)){
+    } else if (!tool.isValidUserName(username)) {
         logger.error(TAG, `Invalid username : ${username}`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
@@ -114,21 +114,19 @@ User.prototype.create = async (account, username, password, passwordConfirmed, a
         ]
     )
     console.log([isUniqueAccount, isUniqueUserName, isUniqueEmail]);
-    if(!isUniqueAccount)
-    {
+    if (!isUniqueAccount) {
         logger.error(TAG, `Invalid account : ${account} already existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if(!isUniqueUserName){
+    } else if (!isUniqueUserName) {
         logger.error(TAG, `Invalid username : ${username} already existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if(!isUniqueEmail){
+    } else if (!isUniqueEmail) {
         logger.error(TAG, `Invalid email : ${email} already existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if(!(adim in config.adim))
-    {
+    } else if (!(adim in config.adim)) {
         logger.error(TAG, `Invalid adimister : ${adim} does not existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
-    } else if(!(deparment in config.department)){
+    } else if (!(deparment in config.department)) {
         logger.error(TAG, `Invalid department : ${deparment} does not existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
@@ -138,40 +136,39 @@ User.prototype.create = async (account, username, password, passwordConfirmed, a
             adim, createtime, active, email, department)
             VALUE (${user_id}, "${account}", "${username}", "${password}", 
             "${adim}", NOW(), false, "${email}", "${deparment}");`
-    db.execute(SQL,{});
+    db.execute(SQL, {});
 }
 
 User.prototype.active = async (user_id) => {
     let check = await db.execute(`SELECT user_id FROM userInfo WHERE user_id = ${user_id}`);
-    if(check.length === 0){
+    if (check.length === 0) {
         logger.error(TAG, `Invalid user_id : ${user_id} does not existed.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
     const SQL = `UPDATE userInfo SET active = true WHERE user_id=${user_id}`;
-    console.log(SQL);
     await db.execute(SQL);
 }
 
 User.prototype.login = async function (account, password) {
     const TAG = '[UserLogin]';
     const logger = new Logger();
-    if(tool.isNull(account, password)){
+    if (tool.isNull(account, password)) {
         logger.error(TAG, `Account: ${account} and password: ${password} can't be empty.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
     const userObj = await User.getUserInfo(account);
-    if(_.isEmpty(userObj)){
+    if (_.isEmpty(userObj)) {
         logger.error(TAG, `User: ${account} isn't exist.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid account or password.');
     }
-    if(password !== userObj.password) {
+    if (password !== userObj.password) {
         logger.error(TAG, 'Password is not matched.');
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid account or password.');
     }
 
 
     const response = {
-        user_id : userObj.id,
+        user_id: userObj.id,
         account: userObj.account,
         email: userObj.email,
         username: userObj.username,
@@ -187,7 +184,7 @@ User.prototype.getUserbyId = async function (id) {
     const TAG = '[GETUSERBYID]'
     const logger = new Logger();
     console.log(this.token)
-    if(!tool.isPositiveInteger(id)){
+    if (!tool.isPositiveInteger(id)) {
         logger.error(TAG, `ID: ${id} is not an integer.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
@@ -203,12 +200,12 @@ User.prototype.getUserbyId = async function (id) {
         return result[0]['COUNT(1)'] > 0 ? false : true;
     })()
 
-    if(!isVaildID){
+    if (!isVaildID) {
         logger.error(TAG, `ID: ${id} doesn't exist.`);
         throw exception.BadRequestError('BAD_REQUEST', 'Invalid parameter.');
     }
     const SQL = `SELECT * FROM userInfo WHERE user_id=${id}`;
-    return await db.execute(SQL,{});
+    return await db.execute(SQL, {});
 }
 
 module.exports = User;
