5d65a6e5e3841f55a4fd7a34225b16dea079dad6
SolabComputer <thomson861106@gmail.com>
2021-05-13 22:11:14 +0800

    Update times.js

--- Backend/routes/controllers/times.js
+++ Backend/routes/controllers/times.js
@@ -1,11 +1,42 @@
 var express = require('express');
 const db = require(global.__MODULE_BASE__ + "database");
 const response = require(global.__MODULE_BASE__ + "response")
-const User = require(global.__MODEL_BASE__ + "User")
+const Time = require(global.__MODEL_BASE__ + "Time")
 const httpStatus = require('http-status-codes');
 const aurthor = require('./Aurthorize');
 var router = express.Router();
+router.use('*', aurthor.doAuthAction);
 
+router.post('/update', async (req, resp) => {
+    const token = req.body.token;
+    const { id, timeString } = req.body;
+    try {
+        const result = await new Time(token).update(id, timeString);
+        return response.succ(resp, result);
+    } catch (err) {
+        return response.fail(resp, err);
+    }
+});
 
+router.delete('/delete', async (req, resp) => {
+    const token = req.body.token;
+    const id = req.body.id || "";
+    try {
+        const result = await new Time(token).delete(id);
+        return response.succ(resp, result);
+    } catch (err) {
+        return response.fail(resp, err);
+    }
+});
+
+router.get('/getALL', async (req, resp) => {
+    const token = req.body.token;
+    try {
+        const result = await new Time(token).getALL();
+        return response.succ(resp, result);
+    } catch (err) {
+        return response.fail(resp, err);
+    }
+});
 
 module.exports = router;
