cddae77e5cd36955ab9d349dc8680b3ce045cc6d
chungct861106 <chungct@solab.me.ntu.edu.tw>
2021-06-04 11:29:28 +0800

    Update SignUp

--- Frontend/src/components/signupModel.js
+++ Frontend/src/components/signupModel.js
@@ -1,6 +1,18 @@
-import React, { useRef, useState } from 'react'
-import { Form, Input, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, Modal } from 'antd'
-import { doSignup } from '../axios'
+import React, { useRef, useState } from "react";
+import {
+  Form,
+  Input,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+  Modal,
+} from "antd";
+import { doSignup } from "../axios";
+import Department from "../department.json";
 
 const { Option } = Select;
 
@@ -20,51 +32,61 @@ const formItemLayout = {
   },
 };
 
-export default function SignupModel(props){
+export default function SignupModel(props) {
+  const departmentRef = useRef();
+  const usernameRef = useRef();
+  const identityRef = useRef();
+  const passwordRef = useRef();
+  const [form] = Form.useForm();
+  const [issignup, setsignup] = useState(false);
+  const [showWarn, setShowWarn] = useState(false);
 
-  const departmentRef = useRef()
-  const usernameRef = useRef()
-  const identityRef = useRef()
-  const passwordRef = useRef()
-  const [ form ] = Form.useForm()
-  const [ issignup, setsignup ] = useState(false)
-  const [ showWarn, setShowWarn ] = useState(false)
-  
-  const handleOK = async() => {
-    if(issignup){ 
-      props.setVisible(false)
-      setsignup(false)
-    }else if(await doSignup(departmentRef.current.value, usernameRef.current.value, identityRef.current.value, passwordRef.current.value)){
-      setsignup(true)
-      setShowWarn(false)
-    }else{
-      setShowWarn(true)
+  const handleOK = async () => {
+    if (issignup) {
+      props.setVisible(false);
+      setsignup(false);
+    } else if (
+      await doSignup(
+        departmentRef.current.value,
+        usernameRef.current.value,
+        identityRef.current.value,
+        passwordRef.current.value
+      )
+    ) {
+      setsignup(true);
+      setShowWarn(false);
+    } else {
+      setShowWarn(true);
     }
-    
-    form.resetFields();
-  }
 
-  const handleCancel = () =>{
-    props.setVisible(false)
-  }
+    form.resetFields();
+  };
+
+  const handleCancel = () => {
+    props.setVisible(false);
+  };
 
   return (
-    <Modal 
-      visible = { props.visible }
-      onOk = { handleOK }
-      onCancel = { handleCancel } 
-      afterClose = {()=>setShowWarn(false)}
-      >
-        {issignup && (<h2 style={{textAlign:"center"}}>註冊成功</h2>)}
-        {!issignup && (
-          <Form
-          {...formItemLayout}
-          name="register"
-          form={form}
+    <Modal
+      visible={props.visible}
+      onOk={handleOK}
+      onCancel={handleCancel}
+      afterClose={() => setShowWarn(false)}
+    >
+      {issignup && <h2 style={{ textAlign: "center" }}>註冊成功</h2>}
+      {!issignup && (
+        <Form {...formItemLayout} name="register" form={form}>
+          <h2 style={{ textAlign: "center" }}>註冊</h2>
+          <h4
+            style={{
+              textAlign: "center",
+              visibility: showWarn ? "" : "hidden",
+              color: "red",
+            }}
           >
-            <h2 style={{textAlign:"center"}}>註冊</h2>
-            <h4 style={{textAlign:"center",visibility:showWarn?"":"hidden",color:"red"}} >註冊失敗!</h4>
-            <Form.Item
+            註冊失敗!
+          </h4>
+          <Form.Item
             name="department"
             label="Department"
             rules={[
@@ -72,31 +94,28 @@ export default function SignupModel(props){
                 required: true,
               },
             ]}
-            >
-              <Select
-                placeholder="Select your department"
-                ref={departmentRef}
-              >
-                  <Option value="d1">d1</Option>
-                  <Option value="d2">d2</Option>
-                  <Option value="d3">d3</Option>
-              </Select>
-            </Form.Item>
+          >
+            <Select placeholder="Select your department" ref={departmentRef}>
+              {Object.keys(Department.info).map((part) => (
+                <Option value={part}>{Department.info[part]["zh"]}</Option>
+              ))}
+            </Select>
+          </Form.Item>
 
-            <Form.Item
-              name="username"
-              label="Username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your username!',
-                },
-              ]}
-            >
-              <Input ref={usernameRef}/>
-            </Form.Item>
+          <Form.Item
+            name="username"
+            label="Username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input ref={usernameRef} />
+          </Form.Item>
 
-            <Form.Item
+          <Form.Item
             name="identity"
             label="Identity"
             rules={[
@@ -104,53 +123,54 @@ export default function SignupModel(props){
                 required: true,
               },
             ]}
-            >
-              <Select
-                placeholder="Select your identity"
-                ref={identityRef}
-              >
-                <Option value="i1">i1</Option>
-                <Option value="i2">i2</Option>
-                <Option value="i3">i3</Option>
-              </Select>
-            </Form.Item>
-            <Form.Item
-              name="password"
-              label="Password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
+          >
+            <Select placeholder="Select your identity" ref={identityRef}>
+              <Option value="adiminister">主辦人員</Option>
+              <Option value="team">系隊代表</Option>
+              <Option value="recorder">紀錄人員</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password ref={passwordRef} />
+          </Form.Item>
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      "The two passwords that you entered do not match!"
+                    )
+                  );
                 },
-              ]}
-              hasFeedback
-            >
-              <Input.Password ref={passwordRef}/>
-            </Form.Item>
-            <Form.Item
-              name="confirm"
-              label="Confirm Password"
-              dependencies={['password']}
-              hasFeedback
-              rules={[
-                {
-                  required: true,
-                  message: 'Please confirm your password!',
-                },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (!value || getFieldValue('password') === value) {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
-                  },
-                }),
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-      </Form>
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Form>
       )}
     </Modal>
   );
-};
+}
