8fb1bc583ecdff3552e008c433655e5bb6174270
IMEVA <55312900+b06705039@users.noreply.github.com>
2021-08-05 17:15:47 +0800

    combine aCycle

--- Frontend/src/components/Cycles.js
+++ Frontend/src/components/Cycles.js
@@ -1,10 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import Acycle3 from "./Acycle3";
-import Acycle4 from "./Acycle4";
+import ACycle from "./drawLots/aCycle";
 import { usePreGame } from "../hooks/usePreGame";
 import { usePages } from "../hooks/usePages";
-// import { Spin } from "antd";
 
 const CycleDiv = styled.div`
   display: inline-flex;
@@ -26,23 +24,14 @@ const Cycles = () => {
       ) : (
         mapDict &&
         Object.entries(mapDict).map((sessionObject, index) => {
-          if (Object.keys(sessionObject[1]).length === 3) {
-            return (
-              <Acycle3
-                key={index}
-                groupSession={sessionObject[0]}
-                data={sessionObject[1]}
-              />
-            );
-          } else {
-            return (
-              <Acycle4
-                key={index}
-                groupSession={sessionObject[0]}
-                data={sessionObject[1]}
-              />
-            );
-          }
+          console.log("cycles:", sessionObject[1]);
+          return (
+            <ACycle
+              key={index}
+              groupSession={sessionObject[0]}
+              data={sessionObject[1]}
+            />
+          );
         })
       )}
     </CycleDiv>
